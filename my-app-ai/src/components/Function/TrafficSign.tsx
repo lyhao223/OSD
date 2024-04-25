d1fe1dad882e4be7450cc18ecb449bc79c3a7ec4
lyhao223 <lyhao0710@gmail.com>
2024-04-25 16:36:49 +0700

    update my-app-ai

--- my-app-ai/src/components/Function/TrafficSign.tsx
+++ my-app-ai/src/components/Function/TrafficSign.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { FaImage } from "react-icons/fa";
 
 const TrafficSignClassification: React.FC = () => {
@@ -13,11 +13,11 @@ const TrafficSignClassification: React.FC = () => {
       setIsLoading(true); // Set loading state to true
 
       const formData = new FormData();
-      formData.append('file', file);
+      formData.append("file", file);
       try {
-        const response = await fetch('http://127.0.0.1:5000/predict', {
-          method: 'POST',
-          body: formData
+        const response = await fetch("http://127.0.0.1:5000/predict", {
+          method: "POST",
+          body: formData,
         });
 
         if (response.ok) {
@@ -25,14 +25,14 @@ const TrafficSignClassification: React.FC = () => {
           const signName = responseData.Sign;
           setClassification(signName);
         } else {
-          throw new Error('Failed to fetch');
+          throw new Error("Failed to fetch");
         }
       } catch (error) {
         console.error(error);
       } finally {
         setTimeout(() => {
           setIsLoading(false); // Hide loading after 2 seconds
-        }, 2000) // Set loading state to false after API call
+        }, 2000); // Set loading state to false after API call
       }
     }
   };
@@ -44,22 +44,47 @@ const TrafficSignClassification: React.FC = () => {
 
   return (
     <div>
-      <h1 className='w-fit text-white px-5 py-3 text-4xl font-merriweather font-normal border rounded-3xl border-violet-800 bg-violet-800 mx-auto mb-12'>
+      <h1 className="w-fit text-white px-5 py-3 text-4xl font-merriweather font-normal border rounded-3xl border-violet-800 bg-violet-800 mx-auto mb-12">
         Know Your Traffic Sign
       </h1>
-      <div className='mb-10 w-full'>     
-        <input type="file" onChange={handleUpload} accept="image/*" id="upload-img" style={{ display: 'none' }} />
-        <label htmlFor="upload-img" className="bg-violet-900 font-merriweather leading-6 text-white px-5 py-3 rounded-2xl text-3xl cursor-pointer shadow-lg duration-200 hover:shadow-violet-500">
-          <FaImage className='inline-block mr-3' />Upload Image 
+      <div className="mb-10 w-full">
+        <input
+          type="file"
+          onChange={handleUpload}
+          accept="image/*"
+          id="upload-img"
+          style={{ display: "none" }}
+        />
+        <label
+          htmlFor="upload-img"
+          className="bg-violet-900 font-merriweather leading-6 text-white px-5 py-3 rounded-2xl text-3xl cursor-pointer shadow-lg duration-200 hover:shadow-violet-500">
+          <FaImage className="inline-block mr-3" />
+          Upload Image
         </label>
         {image && (
-          <button onClick={handleClear} className="bg-red-500 text-white font-merriweather px-5 py-3 rounded-2xl text-3xl cursor-pointer shadow-lg duration-200 hover:shadow-red-700 ml-5">
+          <button
+            onClick={handleClear}
+            className="bg-red-500 text-white font-merriweather px-5 py-3 rounded-2xl text-3xl cursor-pointer shadow-lg duration-200 hover:shadow-red-700 ml-5">
             Clear
           </button>
         )}
       </div>
-          {image && <img src={image} alt="Uploaded" className='mx-auto w-96 h-96 rounded-2xl shadow-2xl mb-10'/>}
-          {isLoading ? (<p className='text-2xl leading-6 font-merriweather mb-12'>Loading...</p>): classification && <p className='text-2xl leading-6 font-merriweather mb-12 text-violet-400'>Result: {classification}</p>}
+      {image && (
+        <img
+          src={image}
+          alt="Uploaded"
+          className="mx-auto w-96 h-96 rounded-2xl shadow-2xl mb-10"
+        />
+      )}
+      {isLoading ? (
+        <p className="text-2xl leading-6 font-merriweather mb-12">Loading...</p>
+      ) : (
+        classification && (
+          <p className="text-2xl leading-6 font-merriweather mb-12 text-violet-400">
+            Result: {classification}
+          </p>
+        )
+      )}
     </div>
   );
 };
--- my-app-ai/src/components/Technology/ListTechnology.tsx
+++ my-app-ai/src/components/Technology/ListTechnology.tsx
@@ -1,27 +1,33 @@
-
 interface ListTechnologyProps {
-    id: string;
-    img: string;
-    title: string;
-    description: string;
+  id: string;
+  img: string;
+  title: string;
+  description: string;
 }
 
-const ListTechnology = ({ id, img, title, description }: ListTechnologyProps) => {
-    return (
-        <>
-        <div key={id} className="flex flex-col items-center space-y-3 mb-20 md:flex-row md:mx-14 md:space-x-8 ">
-            <img src={img} alt="Python" className="w-24 h-24" />
-            <div className="space-y-3 md:space-y-5 ">
-                <h1 className="text-4xl font-merriweather leading-8 font-normal text-white ">{title}</h1>
-                <p className="text-xl font-merriweather leading-8 font-normal text-white">{description}</p>
-            </div>
+const ListTechnology = ({
+  id,
+  img,
+  title,
+  description,
+}: ListTechnologyProps) => {
+  return (
+    <>
+      <div
+        key={id}
+        className="flex flex-col items-center space-y-3 mb-20 md:flex-row md:mx-14 md:space-x-8 ">
+        <img src={img} alt="Python" className="w-24 h-24" />
+        <div className="space-y-3 md:space-y-5 ">
+          <h1 className="text-4xl font-merriweather leading-8 font-normal text-white ">
+            {title}
+          </h1>
+          <p className="text-xl font-merriweather leading-8 font-normal text-white">
+            {description}
+          </p>
         </div>
+      </div>
+    </>
+  );
+};
 
-        
-        </>
-       
-        
-    );
-}
-
-export default ListTechnology
+export default ListTechnology;
